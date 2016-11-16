db7d1eca4a20cf4fc051b92f5a03c70c4b446c0f
EQuimper <quimperemanuel@gmail.com>
2016-11-16 02:25:43 -0500

    Restrict user with plug

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,8 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
   def index(conn, _params) do
     IO.inspect(conn.assigns)
     topics = Repo.all(Topic)

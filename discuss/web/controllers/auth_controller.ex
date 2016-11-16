4e7c64afaff1c689ab95ee9822f72fa3bc39ced6
EQuimper <quimperemanuel@gmail.com>
2016-11-16 01:12:03 -0500

    Create private module for check user email unique

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,7 +2,21 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
+  alias Discuss.User
 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{},user_params)
+
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end

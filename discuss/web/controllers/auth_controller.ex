40111d4230793fcf0a8abce8501616c170e1884a
EQuimper <quimperemanuel@gmail.com>
2016-11-16 00:58:38 -0500

    Add user model and the provider

---
+++ discuss/web/controllers/auth_controller.ex
@@ -0,0 +1,8 @@
+defmodule Discuss.AuthController do
+  use Discuss.Web, :controller
+  plug Ueberauth
+
+  def callback(conn, params) do
+
+  end
+end
--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -1,6 +1,7 @@
 defmodule Discuss.Router do
   use Discuss.Web, :router
 
+  # Look like middleware
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -16,12 +17,14 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    # get "/", TopicController, :index
-    # get "/topics/new", TopicController, :new
-    # post "/topics", TopicController, :create
-    # get "/topics/:id/edit", TopicController, :edit
-    # put "/topics/:id", TopicController, :update
     resources "/", TopicController
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.

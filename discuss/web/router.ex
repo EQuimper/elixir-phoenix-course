b46cde2f2b0211c84e56e21d950979d6b69b3483
EQuimper <quimperemanuel@gmail.com>
2016-11-16 02:01:45 -0500

    Can now logout

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -25,6 +25,7 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end
